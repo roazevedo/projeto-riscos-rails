2c2815fd23e7d6b35644d67625f14463d5670672
Rodrigo Azevedo <rodrigodeazevedosilva@yahoo.com.br>
2024-11-06 13:35:00 -0300

    Devise installed and new fields added to sign_up and update forms.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,16 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    # For additional fields in app/views/devise/registrations/new.html.erb
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :matricula, :orgao, :setor])
+
+    # For additional in app/views/devise/registrations/edit.html.erb
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :matricula, :orgao, :setor])
+  end
 end
